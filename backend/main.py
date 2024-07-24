459190264a553316139bc879b3a23d34a8412953
Sreedhar Shreyas <shreyas.sreedhar@gmail.com>
2024-07-24 00:32:55 -0400

    company details update

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,41 @@
 from typing import Union
-
-from fastapi import FastAPI
+import pandas as pd
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+
+
+def companies_data():
+    return pd.read_csv("../data/companies.csv")
+
+def locations_data():
+    return pd.read_csv("../data/locations.csv")
+
+
+companies_details = companies_data()
+location_details = locations_data()
+
+print("company details")
+print(companies_details.head())  
+print("location details")
+print(location_details.head())  
+
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/api/companies")
+def get_all_companies():
+    companies = companies_details.to_dict(orient="records")
+    return companies
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/api/companies/{company_id}")
+def get_company(company_id: int):
+    # companies_details = companies_data()
+    company = companies_details[companies_details['company_id'] == company_id]
+    if company.empty:
+        raise HTTPException(status_code=404, detail="Company not found")
+    return company.to_dict(orient="records")[0]
